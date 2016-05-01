53b08256e2c0077d47c4014a766db89bab3c9718
Okan <ozok26@gmail.com>
2016-05-01 23:23:43 +0300

    changed license texts

--- Units/UnitFFmpegCommandLineCreator.pas
+++ Units/UnitFFmpegCommandLineCreator.pas
@@ -1,12 +1,12 @@
 { *
-  * Copyright (C) 2011-2015 ozok <ozok26@gmail.com>
+  * Copyright (C) 2011-2016 ozok <ozok26@gmail.com>
   *
   * This file is part of TEncoder.
   *
   * TEncoder is free software: you can redistribute it and/or modify
   * it under the terms of the GNU General Public License as published by
-  * the Free Software Foundation, either version 2 of the License, or
-  * (at your option) any later version.
+  * the Free Software Foundation, either version 2 of the License.
+  * 
   *
   * TEncoder is distributed in the hope that it will be useful,
   * but WITHOUT ANY WARRANTY; without even the implied warranty of
